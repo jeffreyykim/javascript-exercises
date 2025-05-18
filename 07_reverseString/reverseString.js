461fb6dc8f76a7224575c3fd8d34a94f28f0ad14
jeffreyykim <110882794+jeffreyykim@users.noreply.github.com>
2025-05-18 01:49:12 -0700

    Finished 07 reverseString function

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let splitWord = string.split("");
+    let reversedArray = splitWord.reverse();
+    let reversedWord = reversedArray.join("");;
+
+    return reversedWord;
 
 };
 
