b9d122dc931929b6510dcea38b737a480202ab97
jeffreyykim <110882794+jeffreyykim@users.noreply.github.com>
2025-05-20 11:03:50 -0700

    finished question 08 of js exercises

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const final = array.filter(item => !args.includes(item));
+    return final;
 };
 
 // Do not edit below this line
