da20f4e3be1bb168fd24e321f319dd496cab7e4e
jeffreyykim <110882794+jeffreyykim@users.noreply.github.com>
2025-05-20 11:35:32 -0700

    finished 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(floor, ceiling) {
+    if (floor < 0 || ceiling < 0) {
+        return "ERROR";
+    }
+    if (floor > ceiling) {
+        [floor, ceiling] = [ceiling, floor];
+    }
+    if (!Number.isInteger(floor) ||  !Number.isInteger(ceiling)) {
+        return "ERROR";
+    }
+    let summed = 0;
+    for (let i = floor; i <= ceiling; i++) {
+        summed += i;
+    }
+    return summed;
 
 };
 
